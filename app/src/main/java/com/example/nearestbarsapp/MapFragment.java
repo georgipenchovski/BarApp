f8178283e2df35d6d5f714099eab166976d53bdd
georgipenchovski <georgipenchovski@gmail.com>
2020-02-04 01:20:34 +0200

    map in map fragment added

--- app/src/main/java/com/example/nearestbarsapp/MapFragment.java
+++ app/src/main/java/com/example/nearestbarsapp/MapFragment.java
@@ -3,14 +3,31 @@ package com.example.nearestbarsapp;
 import android.os.Bundle;
 import android.view.View;
 
-public class MapFragment extends BaseFragment {
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapFragment extends BaseFragment implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
+
     @Override
     protected int getLayoutRes() {
-       return R.layout.fragment_map;
+        return R.layout.fragment_map;
     }
 
     @Override
     protected View onFragmentViewCreated(View view, Bundle savedInstanceState) {
-        return null;
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
