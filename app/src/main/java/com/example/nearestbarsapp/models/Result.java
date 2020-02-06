2ba723c0be402016417baf9e72832b75dc60c280
georgipenchovski <georgipenchovski@gmail.com>
2020-02-06 16:23:06 +0200

    useless params deleted

--- app/src/main/java/com/example/nearestbarsapp/models/Result.java
+++ app/src/main/java/com/example/nearestbarsapp/models/Result.java
@@ -11,36 +11,18 @@ public class Result {
     @SerializedName("geometry")
     @Expose
     private Geometry geometry;
-    @SerializedName("icon")
-    @Expose
-    private String icon;
     @SerializedName("id")
     @Expose
     private String id;
     @SerializedName("name")
     @Expose
     private String name;
-    @SerializedName("place_id")
-    @Expose
-    private String placeId;
-    @SerializedName("rating")
-    @Expose
-    private Double rating;
-    @SerializedName("reference")
-    @Expose
-    private String reference;
-    @SerializedName("scope")
-    @Expose
-    private String scope;
     @SerializedName("types")
     @Expose
     private List<String> types = new ArrayList<String>();
     @SerializedName("vicinity")
     @Expose
     private String vicinity;
-    @SerializedName("price_level")
-    @Expose
-    private Integer priceLevel;
 
     public Geometry getGeometry() {
         return geometry;
@@ -48,22 +30,6 @@ public class Result {
 
     public void setGeometry(Geometry geometry) {
         this.geometry = geometry;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -74,37 +40,6 @@ public class Result {
         this.name = name;
     }
 
-    public String getPlaceId() {
-        return placeId;
-    }
-
-    public void setPlaceId(String placeId) {
-        this.placeId = placeId;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public void setRating(Double rating) {
-        this.rating = rating;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public void setScope(String scope) {
-        this.scope = scope;
-    }
 
     public List<String> getTypes() {
         return types;
@@ -121,13 +56,4 @@ public class Result {
     public void setVicinity(String vicinity) {
         this.vicinity = vicinity;
     }
-
-    public Integer getPriceLevel() {
-        return priceLevel;
-    }
-
-    public void setPriceLevel(Integer priceLevel) {
-        this.priceLevel = priceLevel;
-    }
-
 }
